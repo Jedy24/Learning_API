6facff7803a9237acab505989dd88277235b4be9
Jedy24 <reihimawari27@gmail.com>
2024-01-15 17:52:45 +0700

    Change alter_users_table

--- database/migrations/2024_01_03_090435_alter_users_table.php
+++ database/migrations/2024_01_03_090435_alter_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone_number', 15)->after('email')->unique();
+            $table->string('phone_number', 15)->nullable()->after('email')->unique();
             $table->date('date_of_birth')->nullable()->after('phone_number');
         });
     }
