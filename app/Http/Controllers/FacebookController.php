9d9a99af596304eb3630006e21b082108824b902
Jedy24 <reihimawari27@gmail.com>
2024-01-16 12:10:06 +0700

    Facebook API login

---
+++ app/Http/Controllers/FacebookController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Exception;
+
+class FacebookController extends Controller
+{
+    public function facebookpage(){
+        return socialite::driver('facebook')->redirect();
+    }
+
+    public function facebookcallback(){
+        try {
+            $user = Socialite::driver('facebook')->user();
+            $finduser = User::where('facebook_id', $user->id)->first();
+            if($finduser){
+                Auth::login($finduser);
+                return redirect()->intended('dashboard');
+            } else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'facebook_id'=> $user->id,
+                    'password' => encrypt('123456789')
+                ]);
+
+                Auth::login($newUser);
+
+                return redirect()->intended('dashboard');
+            }
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
