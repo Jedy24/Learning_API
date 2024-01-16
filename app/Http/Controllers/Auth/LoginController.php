39f95fd1344854f8a66b17ae99bffd7bfd2d59de
Jedy24 <reihimawari27@gmail.com>
2024-01-16 15:49:37 +0700

    Trying JSON

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,36 +5,70 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = '/dashboard';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        if ($request->expectsJson()) {
+            return response()->json([
+                'name' => $user->name,
+                'email' => $user->email,
+            ]);
+        }
+
+        return redirect()->intended($this->redirectPath());
+    }
+
+    /**
+     * Redirect the user to the provider authentication page.
+     *
+     * @param string $provider
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from the provider.
+     *
+     * @param string $provider
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     */
+    public function handleProviderCallback($provider)
+    {
+        try {
+            $user = Socialite::driver($provider)->user();
+        } catch (\Exception $e) {
+            return redirect()->route('login')->withErrors(['socialite' => $e->getMessage()]);
+        }
+
+        // You can customize this part to suit your needs.
+        // In this example, it returns user data as JSON response.
+        return response()->json([
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+            'provider' => $provider,
+        ]);
     }
 }
